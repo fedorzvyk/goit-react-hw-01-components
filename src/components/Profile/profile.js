406aad0b8632e19b0a97f458612a9a1f5b583cf6
fedorzvyk <fedorzvyk@gmail.com>
2022-12-11 21:22:55 +0200

    add component 1

---
+++ src/components/Profile/profile.js
@@ -0,0 +1,42 @@
+// import ReactDOM from 'react-dom';
+
+export default function Profile(props) {
+  console.log(props);
+  const { username, tag, location, avatar, stats } = props;
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt={username} class="avatar" />
+        <p class="name">{username}</p>
+        <p class="tag">@{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+// ReactDOM.render(
+//   <Profile
+//     username={user.username}
+//     tag={user.tag}
+//     location={user.location}
+//     avatar={user.avatar}
+//     stats={user.stats}
+//   />,
+//   document.querySelector('#root')
+// );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// import './components/Profile/profile';
+
 import { App } from 'components/App';
 import './index.css';
 
