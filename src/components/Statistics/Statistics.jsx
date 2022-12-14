52f5c7277842180926663df7df630290b3d1bab0
fedorzvyk <fedorzvyk@gmail.com>
2022-12-14 13:40:56 +0200

    add contrast

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -28,6 +28,8 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+console.log(getRandomHexColor())
+
 Statistics.propTypes = {
   title: PropTypes.string,
   stats: PropTypes.arrayOf(
